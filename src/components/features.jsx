ef9f92ab00ecd17b0676d93c37fb7a080a94090d
Rhys Edwards <edwards.j.rhys@gmail.com>
2020-06-10 14:33:37 +0100

    Added Facebook and Twitter to coverage

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -8,7 +8,7 @@ function Features () {
         <h2 className="w-10/12 md:w-8/12 lg:w-6/12 xl:w-5/12 text-center font-headers self-center text-gray-800 text-2xl lg:text-3xl">Looking to filter out the noise from your internet browsing experience?</h2>
       </div>
       <div className="flex justify-center my-4">
-        <p className="w-10/12 md:w-8/12 lg:w-6/12 xl:w-5/12 text-center font-content self-center text-gray-800 text-lg lg:text-xl">Let&#39;s face it, visiting your average comment section is not just an unwanted distraction, it&#39;s an addictive distraction. That&#39;s why I developed Noise Filter, an extension for your web browser that hides the comment sections from popular news and social media <Link className="text-purple-600" to="/coverage">websites</Link>.</p>
+        <p className="w-10/12 md:w-8/12 lg:w-6/12 xl:w-5/12 text-center font-content self-center text-gray-800 text-lg lg:text-xl">Let&#39;s face it, visiting your average comment section is not just an unwanted distraction, it&#39;s an addictive distraction. That&#39;s why I developed Noise Filter, an extension for your web browser that hides the comment sections from popular news and social media <Link className="text-purple-600" to="/coverage">websites</Link>, including Facebook, Twitter, and Youtube.</p>
       </div>
       <div className="flex justify-center py-8">
         <div className="w-10/12 md:w-7/12 lg:w-10/12 xl:w-8/12 flex flex-wrap lg:flex-no-wrap justify-between">
